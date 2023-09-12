9c6eaf72766199f31765c87faec964ff372a9cc6
Sujant Kumar Kv <sujantdev@gmail.com>
2023-09-12 22:45:18 +0530

    update embd script

--- get_embeddings.py
+++ get_embeddings.py
@@ -16,29 +16,32 @@ def query(texts):
     response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
     return response.json()
 
-
+ct = 0
 # Get embeddings for each line in ipc.jsonl and store in a dataframe
 def embeddings_df(lines):
     embeddings = []
     for line in lines:
+        ct += 1
         data = json.loads(line)
         text = data['text']
         embedding = query(text)
         embeddings.append(embedding)
+        if ct % 100 == 0:
+            print(ct + " steps done")
     # convert & store temporarily as dataframe
-    embeddings_df = pd.DataFrame(ipc_embeddings)
+    embeddings_df = pd.DataFrame(embeddings)
     return embeddings_df
 
 # convert embeddings to pickle, then save it in a directory structure.
 
-ilc_embeddings = pd.DataFrame() # indian_legal_corpus(ilc) embeddings
+# ilc_embeddings = pd.DataFrame() # indian_legal_corpus(ilc) embeddings
 
 with open('indian_legal_corpus.jsonl', 'r') as file:
     lines = file.readlines()
     
 # embeddings with each 4096 chunk
-embeddings_df = embeddings_df(lines)
+ilc_embeddings = embeddings_df(lines)
 # append the new df to the combined df (iteratively)
-ilc_embeddings = pd.concat([ilc_embeddings, embeddings_df], ignore_index=True)
+# ilc_embeddings = pd.concat([ilc_embeddings, embeddings_df], ignore_index=True)
 
 combined_df.to_pickle('ilc_embeddings.pkl')
