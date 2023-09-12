f10903ad3e58a1b5b838e9ebd27b7db2b06606fa
Sujant Kumar Kv <sujantdev@gmail.com>
2023-09-12 18:16:56 +0530

    add getting embeddings script & legal corpus jsonl

---
+++ get_embeddings.py
@@ -0,0 +1,44 @@
+# !pip install pandas
+# !wget indian_legal_corpus.jsonl
+# !mkdir embeddings
+import requests
+import json
+import pandas as pd
+
+model_id = "sujantkumarkv/legalpilot-7b-india-v1.0" # sentence-transformers/all-MiniLM-L6-v2 # sujantkumarkv/legalpilot-7b-india-v1.0
+hf_token = "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"
+
+api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+headers = {"Authorization": f"Bearer {hf_token}"}
+
+
+def query(texts):
+    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
+    return response.json()
+
+
+# Get embeddings for each line in ipc.jsonl and store in a dataframe
+def embeddings_df(lines=lines):
+    embeddings = []
+    for line in lines:
+        data = json.loads(line)
+        text = data['text']
+        embedding = query(text)
+        embeddings.append(embedding)
+    # convert & store temporarily as dataframe
+    embeddings_df = pd.DataFrame(ipc_embeddings)
+    return embeddings_df
+
+# convert embeddings to pickle, then save it in a directory structure.
+
+combined_df = pd.DataFrame()
+
+with open('indian_legal_corpus.jsonl', 'r') as file:
+    lines = file.readlines()
+    
+# embeddings with each 4096 chunk
+embeddings_df = embeddings_df(lines)
+# append the new df to the combined df (iteratively)
+combined_df = pd.concat([combined_df, embeddings_df], ignore_index=True)
+
+combined_df.to_pickle('embeddings/combined_df.pkl')
